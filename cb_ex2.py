41ced234333b39e5139bcf35be866d5f89a7243c
Zorro30 <gbp301195@gmail.com>
2018-06-13 23:41:57 +0530

    Source Code

---
+++ cb_ex2.py
@@ -0,0 +1,27 @@
+def SimpleAdding(num): 
+
+    # code goes here
+    total = 0
+    for i in range(1, num+1):
+        total = total + i
+    return total
+    
+# keep this function call here  
+print (SimpleAdding(int(input("Input the number:"))))  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
