6f3cb30094f4d309f37225a64ccbb1c62c94a46a
Zorro30 <gbp301195@gmail.com>
2018-06-14 00:36:05 +0530

    Source Code

---
+++ cb_ex4.py
@@ -0,0 +1,14 @@
+def SimpleSymbols(str):
+
+    str = '=' + str + '='
+
+    for i in range (0 , len(str)):
+
+        if str[i].isalpha():
+
+            if str[i-1] != '+' or str[i+1] != '+':
+                return 'false'
+                
+    return 'true'
+
+print (SimpleSymbols(input("Enter string:")))
